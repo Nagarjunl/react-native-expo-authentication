61bcd76bf6b3e3d083c2129c39102f8dba43b26c
CP-ThinkDigital <coderplaysmarketing@gmail.com>
2023-04-17 14:26:54 +0530

    authentication done

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -4,30 +4,26 @@ import { Button, TextInput, View } from "react-native";
 import { useDispatch } from "react-redux";
 
 import { addTokens } from "../features/auth/authSlice";
+
+import { useVerifyOtpMutation } from "../services/authAPI";
+
 const SignIn = () => {
   const dispatch = useDispatch();
+
+  const [verifyOtp] = useVerifyOtpMutation();
 
   const [mobilenumber, setMobilenumber] = React.useState("9790056175");
   const [otp, setOtp] = React.useState("590852");
 
   const signIn = (data) => {
-    fetch("http://192.168.1.4:3005/authentication/validate-otp", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        mobile_number: Number(data.mobilenumber),
-        otp_number: Number(data.otp),
-      }),
+    verifyOtp({
+      mobile_number: Number(data.mobilenumber),
+      otp_number: Number(data.otp),
     })
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch(addTokens(json));
-      })
-      .catch((error) => {
-        console.error(error);
+      .unwrap()
+      .then((res) => {
+        const { user, ...rest } = res;
+        dispatch(addTokens(rest));
       });
   };
 
