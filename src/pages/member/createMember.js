b50b9b87b2558f87a77c9389c2e9390910ba21a0
barathan <barathanit7@gmail.com>
2023-04-25 13:48:36 +0530

    add to tha member list and table

--- src/pages/member/createMember.js
+++ src/pages/member/createMember.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Button, TextInput, View, Text } from 'react-native'
+import {
+  StyleSheet,
+  Button,
+  TextInput,
+  View,
+  Text,
+  Modal,
+  Pressable,
+  ScrollView,
+} from 'react-native'
 import {
   usePostMembersMutation,
   useGetMembersQuery,
 } from '../../services/membersAPI'
 import { useForm, Controller } from 'react-hook-form'
-
+import { DataTable } from 'react-native-paper'
 function CreateMember() {
   const {
     control,
@@ -21,6 +30,7 @@ function CreateMember() {
   const [postMember] = usePostMembersMutation()
   const [members, setMembers] = useState([])
   const { data: membersList, isLoading } = useGetMembersQuery()
+  const [modalVisible, setModalVisible] = useState(false)
   const createMember = async (data) => {
     const { name, mobile_number, id_proof } = data
 
@@ -44,59 +54,110 @@ function CreateMember() {
 
   return (
     <>
-      <View style={styles.container}>
-        <View>
-          <Controller
-            control={control}
-            rules={{
-              required: true,
+      <ScrollView>
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has been closed.')
+              setModalVisible(!modalVisible)
             }}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                style={styles.input}
-                placeholder="Name"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-            name="name"
-          />
-          {errors.name && <Text>Name is required.</Text>}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <View>
+                  <Controller
+                    control={control}
+                    rules={{
+                      required: true,
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <TextInput
+                        style={styles.input}
+                        placeholder="Name"
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                    )}
+                    name="name"
+                  />
+                  {errors.name && <Text>Name is required.</Text>}
 
-          <Controller
-            control={control}
-            rules={{
-              required: true,
-            }}
-            render={({ field: { onChange, onBlur, value } }) => (
-              <TextInput
-                style={styles.input}
-                placeholder="Mobile number"
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-              />
-            )}
-            name="mobile_number"
-          />
-          {errors.mobile_number && <Text>Mobile Number is required.</Text>}
-        </View>
-
-        <View style={styles.button}>
-          <Button title="Add a member" onPress={handleSubmit(onSubmit)} />
-        </View>
-
-        <View style={styles.memberList}>
-          {members &&
-            members.map((member, index) => (
-              <View style={styles.border} key={index}>
-                <Text style={styles.paragraph}>{member.name}</Text>
-                <Text style={styles.mobileNumber}>{member.mobile_number}</Text>
+                  <Controller
+                    control={control}
+                    rules={{
+                      required: true,
+                    }}
+                    render={({ field: { onChange, onBlur, value } }) => (
+                      <TextInput
+                        style={styles.input}
+                        placeholder="Mobile number"
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                      />
+                    )}
+                    name="mobile_number"
+                  />
+                  {errors.mobile_number && (
+                    <Text>Mobile Number is required.</Text>
+                  )}
+                </View>
+                <View style={styles.button}>
+                  <Button
+                    title="Add a member"
+                    onPress={handleSubmit(onSubmit)}
+                  />
+                </View>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
               </View>
-            ))}
+            </View>
+          </Modal>
+          <Pressable
+            style={[styles.button, styles.buttonOpen]}
+            onPress={() => setModalVisible(true)}
+          >
+            <Text style={styles.textStyle}>Show Modal</Text>
+          </Pressable>
         </View>
-      </View>
+        <View style={styles.container}>
+          <View style={styles.memberList}>
+            <DataTable>
+              <DataTable.Header style={styles.Header}>
+                <DataTable.Title sortDirection="descending">
+                  Name
+                </DataTable.Title>
+                <DataTable.Title>Mobilenumber</DataTable.Title>
+                <DataTable.Title numeric sortDirection="descending">
+                  Id-Proof
+                </DataTable.Title>
+              </DataTable.Header>
+              {members &&
+                members.map((member, index) => (
+                  <DataTable.Row key={index} style={styles.table}>
+                    <DataTable.Cell style={styles.paragraph}>
+                      {member.name}
+                    </DataTable.Cell>
+                    <DataTable.Cell style={styles.mobileNumber}>
+                      {member.mobile_number}
+                    </DataTable.Cell>
+                    <DataTable.Cell style={styles.mobileNumber}>
+                      {member.id_proof}
+                    </DataTable.Cell>
+                  </DataTable.Row>
+                ))}
+            </DataTable>
+          </View>
+        </View>
+      </ScrollView>
     </>
   )
 }
@@ -109,60 +170,73 @@ const styles = StyleSheet.create({
     borderColor: '#a9a9a9',
     marginTop: 12,
   },
-  button: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    borderRadius: 10,
-  },
-  profileImgContainer: {
-    width: 150,
-    height: 150,
-    marginLeft: 120,
-    borderRadius: 150 / 2,
-    overflow: 'hidden',
-    borderWidth: 3,
-    borderColor: 'red',
-  },
+
   container: {
     flex: 1,
     justifyContent: 'center',
     margin: 4,
+    padding: 5,
     backgroundColor: 'white',
   },
-  fixToText: {
-    justifyContent: 'center',
-    borderRadius: 40,
-  },
+
   memberList: {
     flex: 1,
-    alignItems: 'stretch',
+    alignItems: 'center',
     paddingTop: 40,
     backgroundColor: 'white',
   },
   paragraph: {
-    margin: 7,
     fontSize: 15,
-    textAlign: 'left',
     color: '#34495e',
   },
   mobileNumber: {
-    margin: 7,
+    margin: 4,
     fontSize: 15,
-    textAlign: 'right',
+    color: '#34495e',
+    marginLeft: 9,
+  },
+  table: {
+    margin: 4,
+    fontSize: 15,
     color: '#34495e',
   },
-  border: {
-    borderRadius: 6,
-    elevation: 3,
-    backgroundColor: '#fff',
-    shadowOffset: { width: 1, height: 1 },
-    shadowColor: '#333',
-    shadowOpacity: 0.3,
-    shadowRadius: 2,
-    marginHorizontal: 4,
-    marginVertical: 6,
-    marginHorizontal: 18,
-    marginVertical: 10,
+  Header: {
+    backgroundColor: 'gray',
+    fontFamily: 'bold',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: 'blue',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 })
 
